e7da86d80e7a1b4fb056d0c2e8cd28a13f8ef1e7
Johann Cepeda <johann.cepeda@mail.escuelaing.edu.co>
2021-09-09 10:09:10 -0500

    Project structure

--- src/main/java/edu/escuelaing/arep/calculator/Calculator.java
+++ src/main/java/edu/escuelaing/arep/calculator/Calculator.java
@@ -1,5 +1,27 @@
 package edu.escuelaing.arep.calculator;
 
 public class Calculator {
-    
+
+    private static Calculator instance = new Calculator();
+
+    private Calculator() {
+    }
+
+    public static Calculator getInstance() {
+        return instance;
+    }
+
+    public static String cos(String number) {
+        return "Coseno de " + number + " = " + Math.cos(Double.parseDouble(number));
+    }
+
+    public static String sen(String number) {
+        return "Seno de " + number + " = " + Math.sin(Double.parseDouble(number));
+    }
+
+    public static String tan(String number) {
+
+        return "Tangente de " + number + " = " + Math.tan(Double.parseDouble(number));
+
+    }
 }
